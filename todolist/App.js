1de0137d513990c5245b4b83ad6825ac86c93708
Florence-100 <75921986+Florence-100@users.noreply.github.com>
2021-01-19 22:31:11 +0800

    Delete todolist directory

--- todolist/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {View} from 'react-native';
-import {TodosList} from './src/screens/TodosList';
-
-const App: () => React$Node = () => {
-  return <TodosList />;
-}
-
-export default App;
